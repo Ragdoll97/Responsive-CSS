b83408786aa20af65d19b9ed108b388bfa897fcf
Ragdoll97 <67993025+Ragdoll97@users.noreply.github.com>
2021-04-15 22:50:50 -0400

    Update Portafolio.blade.php

--- resources/views/Portafolio.blade.php
+++ resources/views/Portafolio.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portafolio Personal</title>
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <link rel="stylesheet" href="{{ url('/static/css/portfolio.css?v' . time()) }}">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com">
